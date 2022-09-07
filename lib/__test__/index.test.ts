395bdd89fc8ff3c8f18fa676d3002090b23e3c68
xucong <xuerzong@gmail.com>
2022-09-07 16:59:21 +0800

    chore: add FetchPolling.options.onDone unit test

--- lib/__test__/index.test.ts
+++ lib/__test__/index.test.ts
@@ -1,14 +1,32 @@
 import FetchPolling from '..'
 
+const mockCalledTime = 5
+
 test('`shouldStop` should be able to make the process end', async () => {
   let fetchTime = 0;
   const fetchMock = jest.fn(() => Promise.resolve(fetchTime++))
   const fetchPolling = new FetchPolling({
     fetch: fetchMock,
-    shouldStop: () => fetchTime === 5
+    shouldStop: () => fetchTime === mockCalledTime
   })
 
   await fetchPolling.start()
 
-  expect(fetchMock).toHaveBeenCalledTimes(5)
+  expect(fetchMock).toHaveBeenCalledTimes(mockCalledTime)
+})
+
+
+test('`onDone` should be run when `shouldStop` return true', async () => {
+  let fetchTime = 0;
+  const fetchMock = jest.fn(() => Promise.resolve(fetchTime++))
+  const onDone = jest.fn(res => res)
+  const fetchPolling = new FetchPolling({
+    fetch: fetchMock,
+    shouldStop: () => fetchTime === mockCalledTime,
+    onDone
+  })
+
+  await fetchPolling.start()
+  expect(onDone.mock.results[0].value).toBe(mockCalledTime - 1)
+  expect(onDone).toHaveBeenCalledTimes(1)
 })
